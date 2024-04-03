f70530585620dff83fc859cf7dd1768bb68578ba
WVI11IM <wvi11im97@gmail.com>
2024-04-02 22:48:29 -0300

    Fixed rotation and added braking
    - Character no longer spins when colliding with wall
    - Player can now brake pressing both A and D

--- Assets/Scripts/MovementTest2.cs
+++ Assets/Scripts/MovementTest2.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class MovementTest2 : MonoBehaviour
 {
-    public float maxMoveSpeed = 5;
     public float moveSpeed;
-
-    public float maxRotSpeed = 5;
     public float rotSpeed;
     Rigidbody rb;
 
@@ -18,20 +15,24 @@ public class MovementTest2 : MonoBehaviour
 
     void Update()
     {
-        //if (moveSpeed > maxMoveSpeed) moveSpeed = maxMoveSpeed;
-        //if (rotSpeed > maxRotSpeed) rotSpeed = maxRotSpeed;
+        //Variável local de vetor de velocidade
+        float vertical;
 
-        float vertical = Input.GetAxis("Vertical");
+        //Se teclas A e D estiverem sendo seguradas ao mesmo tempo, personagem desacelera. Senão, segue em frente com velocidade normal
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) vertical = 0;
+        else vertical = 1;
 
+        //Variável local de valor de rotação a adicionar
         float rotation = Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime;
+
+        //Se teclas A e D estiverem sendo seguradas ao mesmo tempo, personagem para de girar para os lados.
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) rotation = 0;
+        
         transform.eulerAngles += new Vector3(0, rotation, 0);
 
         Vector3 direction = new Vector3(0, 0, vertical);
 
         direction = transform.TransformDirection(direction);
         rb.AddForce(direction * moveSpeed);
-
-        Debug.Log("Horizontal: " + Input.GetAxis("Horizontal"));
-        Debug.Log("Vertical: " + Input.GetAxis("Vertical"));
     }
 }
