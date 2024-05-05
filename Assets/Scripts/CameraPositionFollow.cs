9375b2bb046c20d2fa4a52267ab501c2d337fbf8
WVI11IM <wvi11im97@gmail.com>
2024-05-05 16:10:31 -0300

    Modular Level Scene
    - New prefabs for level making
    - Adjustment to camera angle

--- Assets/Scripts/CameraPositionFollow.cs
+++ Assets/Scripts/CameraPositionFollow.cs
@@ -4,11 +4,17 @@ public class CameraPositionFollow : MonoBehaviour
 {
     public Transform playerPosition;
     public float smoothSpeed = 0.125f; // Adjust the smoothness factor
+    private Vector3 initialCameraPosition;
+
+    private void Start()
+    {
+        initialCameraPosition = gameObject.transform.position;
+    }
 
     void FixedUpdate()
     {
         // Calculate the desired position with an offset
-        Vector3 desiredPosition = playerPosition.position + new Vector3(25f, 25f, -25f);
+        Vector3 desiredPosition = playerPosition.position + initialCameraPosition;
 
         // Smoothly move the camera towards the desired position using damping
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
