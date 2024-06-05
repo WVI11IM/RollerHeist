02d23740a0a99a4ae63766e336a16cd88e22924f
Sxless <wallace-salles@live.com>
2024-06-05 14:53:39 -0300

    Troca de Skins

--- Assets/Scripts/Menu/CharacterSelection.cs
+++ Assets/Scripts/Menu/CharacterSelection.cs
@@ -6,37 +6,45 @@ using UnityEngine.SceneManagement;
 public class CharacterSelection : MonoBehaviour
 {
     public GameObject[] characters;
+    public GameObject[] charactersSitting;
     public int selectedCharacter = 0;
 
     void Start()
     {
-      
+      selectedCharacter = PlayerPrefs.GetInt("selectedCharacter", 0);
+      characters[selectedCharacter].SetActive(true);
+      charactersSitting[selectedCharacter].SetActive(true);
+      gameObject.SetActive(false);
     }
 
     public void NextCharacter()
     {
+        charactersSitting[selectedCharacter].SetActive(false);
         characters[selectedCharacter].SetActive(false);
         selectedCharacter = (selectedCharacter + 1) % characters.Length;
         characters[selectedCharacter].SetActive(true);
+        charactersSitting[selectedCharacter].SetActive(true);
         PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
     }
 
     public void PreviousCharacter()
     {
+        
         characters[selectedCharacter].SetActive(false);
+        charactersSitting[selectedCharacter].SetActive(false);
         selectedCharacter--;
         if (selectedCharacter < 0)
         {
             selectedCharacter += characters.Length;
         }
         characters[selectedCharacter].SetActive(true);
+        charactersSitting[selectedCharacter].SetActive(true);
         PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
     }
 
     public void StartGame()
     {
         PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
-        SceneManager.LoadScene(1, LoadSceneMode.Single);
         Debug.Log("CharacterSelection: Jogo iniciado com personagem selecionado e paleta alterada.");
     }
 }
