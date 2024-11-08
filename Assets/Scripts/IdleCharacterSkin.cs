ebbb7c7c8a24f290b17bb52da28e45eadf726a70
WVI11IM <wvi11im97@gmail.com>
2024-11-08 00:19:17 -0300

    new menu kenai animations, cassette icon

--- Assets/Scripts/IdleCharacterSkin.cs
+++ Assets/Scripts/IdleCharacterSkin.cs
@@ -7,12 +7,12 @@ using UnityEngine;
 public class IdleCharacterSkin : MonoBehaviour
 {
    public int animationNumber;
-
+    public int numberOfAnimations;
    public Animator animator;
 
    public void RandomizeAnimationNumber()
    {
-    animationNumber = Random.Range(0,2);
+    animationNumber = Random.Range(0,numberOfAnimations);
 
     animator.SetInteger("number",animationNumber);
    }
