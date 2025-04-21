e63b61ba21924556f973f2729ec32461e0817e79
WVI11IM <wvi11im97@gmail.com>
2025-04-20 22:37:37 -0300

    last prefab preparations, hologram animation, flag physics

--- Assets/Scripts/PropScripts/MainPieceSteal.cs
+++ Assets/Scripts/PropScripts/MainPieceSteal.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MainPieceSteal : MonoBehaviour
 {
     public GameObject normalMainPiece;
     public GameObject stolenTreasure;
     public GameObject stolenTreasureOnPlayer;
+    public NavMeshObstacle navMeshObstacle;
     private bool isStolen = false;
     public float destructionDelay = 15f; // Time in seconds before both glass objects are destroyed
 
     // Start is called before the first frame update
     void Start()
     {
+        navMeshObstacle = GetComponent<NavMeshObstacle>();
+        navMeshObstacle.enabled = true;
         stolenTreasure.SetActive(false);
         normalMainPiece.SetActive(true);
         stolenTreasureOnPlayer.SetActive(false);
@@ -25,6 +29,7 @@ public class MainPieceSteal : MonoBehaviour
             stolenTreasure.SetActive(true);
             normalMainPiece.SetActive(false);
             stolenTreasureOnPlayer.SetActive(true);
+            navMeshObstacle.enabled = false;
             isStolen = true;
         }
     }
